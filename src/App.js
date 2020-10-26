779b590ccd0d0020f75f737f22f3146b91447828
Matthew Ognibene <mattognibene1@gmail.com>
2020-10-25 20:24:42 -0400

    stupid fucking callback

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 
 export const authEndpoint = 'https://accounts.spotify.com/authorize';
 const clientId = "62c2f22951254954a357ada4001660b6";
-const redirectUri =  'http://localhost:3000';//"https://mattognibene.github.io/pokeplaylistv3";
+const redirectUri =  "https://mattognibene.github.io/pokeplaylistv3";
 const scopes = [
     "user-top-read"
 ];
