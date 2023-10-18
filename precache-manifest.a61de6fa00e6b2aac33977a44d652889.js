3bffe0f4cb490518efdf8e40ddff3e505319eda8
Matt Ognibene <mognibene@hubspot.com>
2023-10-17 21:45:19 -0400

    Updates

---
+++ precache-manifest.a61de6fa00e6b2aac33977a44d652889.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c03855408bb6364fd7082d2545acac",
+    "url": "/pokeplaylistv3/index.html"
+  },
+  {
+    "revision": "e62084c5ece60b77f2c0",
+    "url": "/pokeplaylistv3/static/css/main.1d73c2c7.chunk.css"
+  },
+  {
+    "revision": "293c9a6163e13d25f012",
+    "url": "/pokeplaylistv3/static/js/2.46aaf102.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/pokeplaylistv3/static/js/2.46aaf102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e62084c5ece60b77f2c0",
+    "url": "/pokeplaylistv3/static/js/main.660b56de.chunk.js"
+  },
+  {
+    "revision": "0b2beeba2e6255bf2848",
+    "url": "/pokeplaylistv3/static/js/runtime-main.e321893c.js"
+  },
+  {
+    "revision": "e06fbc21585ba16b81368353f7d47d0e",
+    "url": "/pokeplaylistv3/static/media/electric.e06fbc21.png"
+  },
+  {
+    "revision": "d549c1068a837adbce49d3c3085e2be8",
+    "url": "/pokeplaylistv3/static/media/fire.d549c106.png"
+  },
+  {
+    "revision": "6d52f2a50aa28e9369278c3fb5c77866",
+    "url": "/pokeplaylistv3/static/media/ghost.6d52f2a5.png"
+  },
+  {
+    "revision": "6d7f7dd77c2d4c09c0e67c82359e135b",
+    "url": "/pokeplaylistv3/static/media/grass.6d7f7dd7.png"
+  },
+  {
+    "revision": "1789d16fb66ec4e8fb01e8b5bc362e50",
+    "url": "/pokeplaylistv3/static/media/metal.1789d16f.png"
+  },
+  {
+    "revision": "f51fc0abba5e4da88859dffc690ab3e1",
+    "url": "/pokeplaylistv3/static/media/normal.f51fc0ab.png"
+  },
+  {
+    "revision": "236f1b4ab0a90b8b58364d83ed0b2ea7",
+    "url": "/pokeplaylistv3/static/media/psychic.236f1b4a.png"
+  },
+  {
+    "revision": "ec3ee3257048950d7020e62a83efec7f",
+    "url": "/pokeplaylistv3/static/media/resistance.ec3ee325.png"
+  },
+  {
+    "revision": "55e73a9b21123a0be14c4cc6b47a6bd9",
+    "url": "/pokeplaylistv3/static/media/sample.55e73a9b.png"
+  },
+  {
+    "revision": "e969b5745f5d86aa62ada1392ac26eb9",
+    "url": "/pokeplaylistv3/static/media/water.e969b574.png"
+  }
+]);
